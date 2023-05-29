0aa432460cc0a6227e336769ff2fd08014ba5a8f
CD-Z <p_loser_Austria@gmx.at>
2023-05-29 14:43:34 +0200

    added Types

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 import EpubBuilder from "./src/main";
+import {
+  Parameter,
+  EpubChapter,
+  File,
+  EpubSettings,
+} from "@cd-z/epub-constructor";
 
-export default EpubBuilder
+export default EpubBuilder;
+export type { Parameter, EpubChapter, File, EpubSettings };
